464aa1c7bb320f6b354293ca21c1e29f4625494c
Brarian <b.namonywa@gmail.com>
2019-02-06 17:30:51 -0600

    tried mapping name and price

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const puppeteer = require("puppeteer");
 
+// python to go through pages sequntially ?
+//map constants 
+//figure out how to turn map to json file 
+//json to sql 
+
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const url = "https://www.freepeople.com/dresses/";
   await page.goto(url);
-  //  await page.screenshot({ path: "example.png" });
+   await page.screenshot({ path: "example.png" });
 
-  const dressNames = await page.evaluate(() =>
-    Array.from(document.querySelectorAll(".c-product-tile__h3")).map(
-      dressName => dressName.innerText.trim()
-    )
-  );
+//   const dressNames = await page.evaluate(() =>
+//     Array.from(document.querySelectorAll(".c-product-tile__h3")).map(
+//       dressName => dressName.innerText.trim()
+//     )
+//   );
 
-  const prices = await page.evaluate(() =>
-    Array.from(document.querySelectorAll(".c-product-tile__price")).map(
- price=> price.innerText.trim()
-    )
-  );
-  //spread the
-  //console.log([...dressNames]);
-  console.log([...prices]);
+//   const prices = await page.evaluate(() => 
+//     Array.from(document.querySelectorAll(".c-product-tile__price")).map(
+//  price=> price.innerText.trim()
+//     )
+//   );
 
-  //console.log(price);
+const list = await page.evaluate(() => {
+  let dressNames = document.querySelectorAll(".c-prodct-tile__h3");
+  let prices = document.querySelectorAll(".c-product-tile__price");
+  let dAndPriceArray = [];
 
-  //console.log(innerText);
-
+  for (let i = 0; i < dressNames.length; i++){
+    dAndPriceArray[i] = {
+      dressNames: dressNames[i].innerText.trim(),
+      prices: prices[i].innerText.trim()
+    };
+  }
+   console.log([...dAndPriceArray]);
+})
+ 
+  // console.log(dressNames);
+  console.log(list);
+  //log([...list]);
+  
   await browser.close();
 })();
