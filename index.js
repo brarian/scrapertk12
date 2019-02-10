5de09f0daf433810ad58bbeb4903ab1e47163db8
Brarian <b.namonywa@gmail.com>
2019-02-10 11:46:51 -0600

    getting entire episode page text

--- index.js
+++ index.js
@@ -9,38 +9,38 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = "https://www.freepeople.com/dresses/";
+  const url = "http://www.k12engineering.net/transcripts/ep2.html";
   await page.goto(url);
-   await page.screenshot({ path: "example.png" });
+//   await page.screenshot({ path: "example.png" });
 
-//   const dressNames = await page.evaluate(() =>
-//     Array.from(document.querySelectorAll(".c-product-tile__h3")).map(
-//       dressName => dressName.innerText.trim()
-//     )
-//   );
+  const dressNames = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".sectionText")).map(
+      dressName => dressName.innerText.trim()
+    )
+  );
 
-//   const prices = await page.evaluate(() => 
-//     Array.from(document.querySelectorAll(".c-product-tile__price")).map(
-//  price=> price.innerText.trim()
-//     )
-//   );
+  const prices = await page.evaluate(() => 
+    Array.from(document.querySelectorAll(".c-product-tile__price")).map(
+ price=> price.innerText.trim()
+    )
+  );
 
-const list = await page.evaluate(() => {
-  let dressNames = document.querySelectorAll(".c-prodct-tile__h3");
-  let prices = document.querySelectorAll(".c-product-tile__price");
-  let dAndPriceArray = [];
+// const list = await page.evaluate(() => {
+//   let dressNames = document.querySelectorAll(");
+//   let prices = document.querySelectorAll(".c-product-tile__price");
+//   let dAndPriceArray = [];
 
-  for (let i = 0; i < dressNames.length; i++){
-    dAndPriceArray[i] = {
-      dressNames: dressNames[i].innerText.trim(),
-      prices: prices[i].innerText.trim()
-    };
-  }
-   console.log([...dAndPriceArray]);
-})
+//   for (let i = 0; i < dressNames.length; i++){
+//     dAndPriceArray[i] = {
+//       dressNames: dressNames[i].innerText.trim(),
+//       prices: prices[i].innerText.trim()
+//     };
+//   }
+//    console.log([...dAndPriceArray]);
+// })
  
-  // console.log(dressNames);
-  console.log(list);
+  console.log([...dressNames]);
+  //console.log(list);
   //log([...list]);
   
   await browser.close();
